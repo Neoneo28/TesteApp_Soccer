22277d421e371df4b3f4f9ce384609b224d5f746
Neoneo28 <fakihallouche@gmail.com>
2022-07-10 17:13:15 -0300

    Soccer

---
+++ app/src/main/java/com/neoneo28/Soccer/MainActivity.java
@@ -0,0 +1,20 @@
+package com.neoneo28.Soccer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    {
+
+    }
+}
